fa245277ed15c1f97fe1f76c8976218a9f7db339
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-05-16 11:55:14 +0500

    error file main.py completed now solve it for Day 1

---
+++ week-1-foundation/day-01-hello-world/main.py
@@ -0,0 +1,39 @@
+#Solve all the error in this file :)
+
+print("=====================================================")
+   print("\tASCII TYPE BIO PRINTER")
+print("======================================================")
+
+name1 = HAMID"
+class = "First year"
+city = "Karachi"
+language = "Urdu"
+Gender = "Male"
+print("-------------------------------")
+print("My Name is:"name)
+print("-------------------------------")
+print("I am in:",class1)
+print("-------------------------------")
+print(I Live in :",city)
+print("-------------------------------")
+print("My native language is:",Language)
+print("-------------------------------")
+print("Gender:",Gender);
+printf("-------------------------------")
+
+
+print("======================================================")
+print("\tF String Version")
+print("======================================================")
+
+print("-------------------------------")
+print("My Name is:{name}")
+print("-------------------------------")
+  print(f"I am in:{class1}")
+print("-------------------------------")
+print(f"I Live in :[city]")
+print("-------------------------------")
+print(f"My native language is:{language}")
+print("-------------------------------")
+print(f"Gender:{Gende}")
+print("-------------------------------")
