f427eabd38008dbd55f57e6c35a2b31bf3f07146
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-05-16 11:53:29 +0500

    exercise.py Day 1 completed

---
+++ week-1-foundation/day-01-hello-world/exercises.py
@@ -0,0 +1,38 @@
+# MAIN PROGRAM - Run this file to see the output.This program is written by repo owner for your help to solve main.py file errors 
+print("======================================================")
+print("\t ASCII TYPE BIO PRINTER")
+print("======================================================")
+
+name = "HAMID"
+class1 = "First year"
+city = "Karachi"
+language = "Urdu"
+Gender = "Male"
+print("-------------------------------")
+print("My Name is:",name)
+print("-------------------------------")
+print("I am in:",class1)
+print("-------------------------------")
+print("I Live in :",city)
+print("-------------------------------")
+print("My native language is:",language)
+print("-------------------------------")
+print("Gender:",Gender)
+print("-------------------------------")
+
+
+print("======================================================")
+print("\t F String Version")
+print("======================================================")
+
+print("-------------------------------")
+print(f"My Name is:{name}")
+print("-------------------------------")
+print(f"I am in:{class1}")
+print("-------------------------------")
+print(f"I Live in :{city}")
+print("-------------------------------")
+print(f"My native language is:{language}")
+print("-------------------------------")
+print(f"Gender:{Gender}")
+print("-------------------------------")
