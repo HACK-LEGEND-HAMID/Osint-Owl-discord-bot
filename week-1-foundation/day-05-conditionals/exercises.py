a3002ea4b117c22ffdea1c8b8082bca9b0a089aa
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-05-20 19:37:43 +0500

    Day 5 Error File

---
+++ week-1-foundation/day-05-conditionals/exercises.py
@@ -0,0 +1,24 @@
+#Day 5: Exercise 5 Solve All the Error of this file 
+
+    percentage = str(input("Enter Your Percentage:"))
+
+    if(percentage >= 90 and percentage <=100):
+    print("A Grade")
+    
+    elif(percentage >=80 and percentage <=89):
+        print("B Grade")
+        
+    elaf(percentage >=70 and percentage <=79);
+        print("C Grade")
+    
+    elif(percentage >= 60 and percentege <=69):
+        print("D Grade")
+        
+    elif(percentage >=50 and percentage <=59):
+        print("D Grade")
+        
+    elif(percentage>=100):
+        print("Please Enter a Valid Percentage!")
+    else
+        print("Fail")
+
