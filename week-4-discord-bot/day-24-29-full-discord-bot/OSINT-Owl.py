17cd94f53a25a333338c028c7e6016ecb96f6c29
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-20 13:39:29 +0500

    New cog added

--- week-4-discord-bot/day-24-29-full-discord-bot/OSINT-Owl.py
+++ week-4-discord-bot/day-24-29-full-discord-bot/OSINT-Owl.py
@@ -23,6 +23,7 @@ class MyBot(commands.Bot):
         await self.load_extension("cogs.mhakai")
         await self.load_extension("cogs.lua_res")
         await self.load_extension("cogs.python_res")
+        await self.load_extension("cogs.ctf")
         await self.tree.sync()
 
 bot = MyBot()
