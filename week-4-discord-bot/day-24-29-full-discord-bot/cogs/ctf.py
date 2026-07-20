a002d6a44358b06c5f6bfbe1cd35eef40848a066
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-20 13:39:55 +0500

    working on ctf function

--- week-4-discord-bot/day-24-29-full-discord-bot/cogs/ctf.py
+++ week-4-discord-bot/day-24-29-full-discord-bot/cogs/ctf.py
@@ -6,7 +6,9 @@ class CTF(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.submission = {}
-       
+        self.ctf_ative = False 
+
     @app_commands.command(name="ctf", description="Use this Command for CTF Task")
-    async def ctf(self, interaction: discord.Interaction):
+    async def ctf(self, interaction: discord.Interaction, name: str, age: int):
+        await interaction.response.send_message(f"My name is {name} and Age is {age}")
     
